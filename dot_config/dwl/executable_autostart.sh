ca7833852b00ff3c891877ee6ffbfbf08b780694
SoulByte07 <soul3147@proton.me>
2026-02-15 11:55:22 +0530

    dwl.autostart: added swaybg

--- dot_config/dwl/executable_autostart.sh
+++ dot_config/dwl/executable_autostart.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Status bar
-dwlb &
-swww-daemon &
+dwlb -font "JetBrainsMono Nerd Font:size=11"
 # Wallpaper (assuming swww is installed)
-swww img /home/soul/Pictures/wallpapers/Anime-Ladys/1377788.png &
-dwlb -ipc &
+swaybg -i /home/soul/Pictures/wallpapers/Anime-Ladys/1377788.png -m fill
 # Notification daemon
 #mako &
