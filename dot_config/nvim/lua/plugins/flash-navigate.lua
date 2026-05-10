d55cd6d4a76a26a6c204740995a2077063a81293
soulbyte07 <soul3147@proton.me>
2026-05-10 09:50:06 +0530

    nvim: ~ flash.nvim: ~ keybinding: s -> f

--- dot_config/nvim/lua/plugins/flash-navigate.lua
+++ dot_config/nvim/lua/plugins/flash-navigate.lua
@@ -2,8 +2,16 @@
 
 return {
   "folke/flash.nvim",
+  event = "VeryLazy",
   opts = {},
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+    { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
   },
+  config = function(_, opts)
+    require("flash").setup(opts)
+    -- Explicitly remap f to flash.jump if lazy-loading fails to take over
+    vim.keymap.set({ "n", "x", "o" }, "f", function()
+      require("flash").jump()
+    end, { desc = "Flash Jump" })
+  end,
 }
